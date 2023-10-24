c1be9600eea94821ecbc3c2cec9d732c7845c4ca
eduardoudg <1510.lalo.51@gmail.com>
2023-10-24 00:02:49 -0600

    main

---
+++ src/test/java/com/udg/mirutina/MiRutinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udg.mirutina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiRutinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
