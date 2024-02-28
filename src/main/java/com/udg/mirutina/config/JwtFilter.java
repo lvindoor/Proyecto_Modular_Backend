ed8113e0a1d28d40a5932e68fd26c87a29649e6a
eduardoudg <1510.lalo.51@gmail.com>
2024-02-28 11:59:50 -0600

    update - syso remomoved

--- src/main/java/com/udg/mirutina/config/JwtFilter.java
+++ src/main/java/com/udg/mirutina/config/JwtFilter.java
@@ -57,7 +57,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        System.out.println("-> " + authenticationToken);
+        // System.out.println("-> " + authenticationToken);
         filterChain.doFilter(request, response);
   }
 
