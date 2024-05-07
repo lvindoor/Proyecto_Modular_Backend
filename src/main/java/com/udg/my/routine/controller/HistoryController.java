5817d63a0c967ff5995616d863a9a865af6fa363
eduardo_dorado <eduardo.dorado@sanmina.com>
2024-05-07 12:13:52 -0600

    update type of response added

--- src/main/java/com/udg/my/routine/controller/HistoryController.java
+++ src/main/java/com/udg/my/routine/controller/HistoryController.java
@@ -1,6 +1,5 @@
 package com.udg.my.routine.controller;
 
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -29,13 +28,13 @@ import com.udg.my.routine.service.ResponseService;
 @RestController
 @RequestMapping("/api/history")
 public class HistoryController {
-	
+
 	@Autowired
 	private HistoryService historyService;
 
 	@Autowired
 	private PassengerService passengerService;
-	
+
 	@Autowired
 	private StationService stationService;
 
@@ -48,17 +47,19 @@ public class HistoryController {
 			Object response = historyService.findAll();
 			return new ResponseEntity<Response>(new Response(true, "Success", response), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Response> getById(@PathVariable Long id) {
 		try {
 			Object response = historyService.findById(id);
 			return new ResponseEntity<Response>(new Response(true, "Success", response), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
@@ -67,53 +68,67 @@ public class HistoryController {
 		try {
 			History response = historyService.findByPassengerId(passengerId);
 
-			if( response == null ) {
+			if (response == null) {
 				return new ResponseEntity<Response>(new Response(false, "Error ", null), HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 
 			return new ResponseEntity<Response>(new Response(true, "Success", Arrays.asList(response)), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	@GetMapping("/completed-challenge")
-    public ResponseEntity<List<History>> getAllWhoCompletedChallengeToday() {
-        List<History> records = historyService.findAllWhoCompletedChallengeToday();
-        return ResponseEntity.ok(records);
-    }
+	public ResponseEntity<Response> getAllWhoCompletedChallengeToday() {
+		try {
+			List<History> records = historyService.findAllWhoCompletedChallengeToday();
+
+			if (records.size() == 0) {
+				new Response(true, "There are not data currently", Arrays.asList());
+			}
+
+			return new ResponseEntity<Response>(new Response(true, "Success", records), HttpStatus.OK);
+		} catch (Exception e) {
+			return responseService.errorResponse("There was an error to get complete");
+		}
+	}
 
 	@GetMapping("/challenge-completed/{passengerId}")
 	public boolean hasPassengerCompletedChallengeToday(@PathVariable Long passengerId) {
 		return historyService.hasCompletedChallenge(passengerId);
 	}
-	
+
 	@PostMapping("/new/{passengerId}/{stationId}")
-	public ResponseEntity<Response> create(@PathVariable Long passengerId, @PathVariable Long stationId, @RequestBody History history) {
+	public ResponseEntity<Response> create(@PathVariable Long passengerId, @PathVariable Long stationId,
+			@RequestBody History history) {
 		try {
 
 			Optional<Passenger> passenger = this.passengerService.findById(passengerId);
 			if (!passenger.isPresent()) {
 				String message = Passenger.class.getSimpleName() + " with id: " + passengerId + " not found";
-				return new ResponseEntity<Response>(new Response(false, "error", this.responseService.errors(message)), HttpStatus.INTERNAL_SERVER_ERROR);
+				return new ResponseEntity<Response>(new Response(false, "error", this.responseService.errors(message)),
+						HttpStatus.INTERNAL_SERVER_ERROR);
 			}
-			
+
 			Optional<Station> station = this.stationService.findById(stationId);
 			if (!station.isPresent()) {
 				String message = Station.class.getSimpleName() + " with id: " + stationId + " not found";
-				return new ResponseEntity<Response>(new Response(false, "error", this.responseService.errors(message)), HttpStatus.INTERNAL_SERVER_ERROR);
+				return new ResponseEntity<Response>(new Response(false, "error", this.responseService.errors(message)),
+						HttpStatus.INTERNAL_SERVER_ERROR);
 			}
-			
+
 			history.setPassenger(passenger.get());
 			history.setStation(station.get());
 
 			Object response = historyService.save(history);
 			return new ResponseEntity<Response>(new Response(true, "Success", response), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	@PutMapping
 	public ResponseEntity<Response> update(@RequestBody History history) {
 		try {
@@ -121,17 +136,19 @@ public class HistoryController {
 			return new ResponseEntity<Response>(new Response(true, "Success", response), HttpStatus.OK);
 
 		} catch (Exception e) {
-			return new ResponseEntity<Response>(new Response(false, "Error: " + e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<Response>(new Response(false, "Error: " + e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Response> delete(@PathVariable Long id) {
-		try {							
-			historyService.deleteById(id);			
+		try {
+			historyService.deleteById(id);
 			return new ResponseEntity<Response>(new Response(true, "Success", null), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<Response>(new Response(false, "Error " + e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
